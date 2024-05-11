9cae7ed02263432cef1c7d596e391dbaa8ae0b9a
Loi Chyan <loichyan@foxmail.com>
2024-05-11 12:07:14 +0800

    refactor(plugins): ungroup `plugins._core`

---
+++ lua/deltavim/plugins/init.lua
@@ -0,0 +1,33 @@
+require("deltavim").init()
+
+return {
+  { "folke/lazy.nvim", dir = vim.env.LAZY },
+  { "loichyan/DeltaVim", lazy = false, priority = 10000 },
+
+  {
+    "AstroNvim/astrocore",
+    lazy = false,
+    priority = 10000,
+    dependencies = { "AstroNvim/astroui" },
+    opts = function()
+      return {
+        autocmds = {},
+        mappings = require("astrocore").empty_map_table(),
+        options = {},
+      }
+    end,
+  },
+
+  {
+    "AstroNvim/astrolsp",
+    lazy = true,
+    opts = function()
+      return {
+        autocmds = {},
+        mappings = require("astrocore").empty_map_table(),
+      }
+    end,
+  },
+
+  { "AstroNvim/astroui", lazy = true },
+}
