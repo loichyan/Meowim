b699c6dd7021773d8c68de6a4357c408af80abf6
Loi Chyan <loichyan@foxmail.com>
2024-05-11 09:31:33 +0800

    refactor(plugins): ungroup `plugins.editor`

---
+++ lua/deltavim/plugins/indent-blankline.lua
@@ -0,0 +1,44 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  event = "User AstroFile",
+  cmd = {
+    "IBLEnable",
+    "IBLDisable",
+    "IBLToggle",
+    "IBLEnableScope",
+    "IBLDisableScope",
+    "IBLToggleScope",
+  },
+  opts = {
+    indent = { char = "▏" },
+    scope = {
+      enabled = true,
+      show_start = false,
+      show_end = false,
+    },
+    exclude = {
+      buftypes = {
+        "nofile",
+        "prompt",
+        "quickfix",
+        "terminal",
+      },
+      filetypes = {
+        "aerial",
+        "alpha",
+        "dashboard",
+        "help",
+        "lazy",
+        "mason",
+        "neo-tree",
+        "NvimTree",
+        "neogitstatus",
+        "notify",
+        "startify",
+        "toggleterm",
+        "Trouble",
+      },
+    },
+  },
+}
---
+++ lua/deltavim/plugins/todo-comments.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/todo-comments.nvim",
+  event = "User AstroFile",
+  cmd = { "TodoTrouble", "TodoTelescope", "TodoLocList", "TodoQuickFix" },
+  opts = {
+    highlight = { multiline = true },
+  },
+}
