b4c49889a61ed3e0ae02c69a58a64ea8ddce36aa
Loi Chyan <loichyan@foxmail.com>
2025-06-16 00:34:27 +0000

    feat(keymaps): use `Ctrl+U/D` to scroll floating widows

--- lua/meowim/plugins/mini/completion.lua
+++ lua/meowim/plugins/mini/completion.lua
@@ -6,6 +6,8 @@ return {
         vim.o.completeopt = "menuone,noinsert,fuzzy"
         local completion = require("mini.completion")
         completion.setup({
+            -- "omnifunc" is required if we set `scroll_up` to <C-u>
+            lsp_completion = { source_func = "omnifunc" },
             window = {
                 info = { border = "solid" },
                 signature = { border = "solid" },
@@ -14,8 +16,8 @@ return {
             mappings = {
                 force_twostep  = "<C-Space>",
                 force_fallback = "<M-Space>",
-                scroll_down    = "<C-f>",
-                scroll_up      = "<C-b>",
+                scroll_down    = "<C-d>",
+                scroll_up      = "<C-u>",
             },
         })
         vim.lsp.config("*", { capabilities = MiniCompletion.get_lsp_capabilities() })
