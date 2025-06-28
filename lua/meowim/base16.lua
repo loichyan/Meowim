0fbe174a0c8989c54a8c307ad52bd374ad41a476
Loi Chyan <loichyan@foxmail.com>
2025-06-28 21:45:55 +0800

    feat(base16): make hunk previews easier to recognize

--- lua/meowim/base16.lua
+++ lua/meowim/base16.lua
@@ -49,6 +49,7 @@ function Base16.colors_customizations(opts, colors)
         colors = colors:add_transparency()
     end
 
+    local lighten = require("meowim.utils").lighten
     local palette = opts.palette
     local get = function(name)
         return colors.groups[name] --[[@as vim.api.keyset.highlight]]
@@ -98,6 +99,13 @@ function Base16.colors_customizations(opts, colors)
 
         ["MiniIndentscopeSymbol"]    = { fg = palette.base04 },
         ["MiniIndentscopeSymbolOff"] = { fg = palette.base04 },
+
+        ["MiniDiffOverAdd"]        = { fg = palette.base05, bg = lighten(palette.base0B, -37) },
+        ["MiniDiffOverDelete"]     = { fg = palette.base05, bg = lighten(palette.base08, -37) },
+        ["MiniDiffOverChange"]     = { fg = palette.base05, bg = lighten(palette.base08, -37) },
+        ["MiniDiffOverChangeBuf"]  = { fg = palette.base05, bg = lighten(palette.base0B, -37) },
+        ["MiniDiffOverContext"]    = { bg = palette.base01, fg = lighten(palette.base08, -23) },
+        ["MiniDiffOverContextBuf"] = {},
     }
     for name, hl in pairs(overrides) do
         colors.groups[name] = hl
