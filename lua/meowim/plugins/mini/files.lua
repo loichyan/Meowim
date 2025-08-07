8ce8f1279f10ed9f3f21a0c8d782b0d955e45eb9
Loi Chyan <loichyan@foxmail.com>
2025-08-07 21:57:31 +0800

    refactor(keymaps): move keymap helpers into spec modules if possible

--- lua/meowim/plugins/mini/files.lua
+++ lua/meowim/plugins/mini/files.lua
@@ -1,22 +1,11 @@
 ---@type MeoSpec
 local Spec = { "mini.files", event = "VeryLazy" }
-
----@param scope "current"|"all"
-local open = function(scope)
-  local cwd, path = vim.fn.getcwd()
-  if scope == "current" then
-    path = vim.api.nvim_buf_get_name(0)
-    path = vim.uv.fs_stat(path) and path or nil
-  end
-  local dir = path or require("meowim.utils").get_git_repo(cwd) or cwd
-  require("mini.files").open(dir)
-end
+local M = {}
 
 Spec.config = function()
   require("mini.files").setup({
     options = { use_as_default_explorer = true },
   })
-
   Meow.autocmd("meowim.plugins.mini.files", {
     {
       event = "FileType",
@@ -25,12 +14,19 @@ Spec.config = function()
       command = "setlocal iskeyword-=_",
     },
   })
-
-  -- stylua: ignore
-  Meow.keymap({
-    { "<Leader>e", function() open("current") end, desc = "Explore file directory" },
-    { "<Leader>E", function() open("all") end,     desc = "Explore project root"   },
-  })
 end
 
-return Spec
+---Opens the explorer at the specified location.
+---@param scope "buffer"|"workspace"
+function M.open(scope)
+  local cwd, path = vim.fn.getcwd()
+  if scope == "buffer" then
+    path = vim.api.nvim_buf_get_name(0)
+    path = vim.uv.fs_stat(path) and path or nil
+  end
+  local dir = path or require("meowim.utils").get_git_repo(cwd) or cwd
+  require("mini.files").open(dir)
+end
+
+M[1] = Spec
+return M
