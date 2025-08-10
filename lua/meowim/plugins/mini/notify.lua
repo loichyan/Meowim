ae72b4dc1f54d41e86e49ba2e9e4886e0a07ee33
Loi Chyan <loichyan@foxmail.com>
2025-08-10 10:59:14 +0800

    feat(mini.notify): make notification window have a stable width

--- lua/meowim/plugins/mini/notify.lua
+++ lua/meowim/plugins/mini/notify.lua
@@ -3,7 +3,14 @@ local Spec = { "mini.notify", lazy = false, priority = 95 }
 
 Spec.config = function()
   local mininotify = require("mini.notify")
-  mininotify.setup()
+  mininotify.setup({
+    window = {
+      config = function()
+        local screen = math.floor(0.5 * vim.o.columns)
+        return { width = math.min(screen, 50) }
+      end,
+    },
+  })
   vim.notify = mininotify.make_notify()
 end
 
