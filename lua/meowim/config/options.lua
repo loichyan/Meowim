75e0e8f9fa3e111bba77ca01984a30a9ef332dbf
Loi Chyan <loichyan@outlook.com>
2026-01-13 17:36:12 +0800

    feat(options): enable fold columns on Neovim 0.12

--- lua/meowim/config/options.lua
+++ lua/meowim/config/options.lua
@@ -20,3 +20,15 @@ o.relativenumber = true -- Show relative numbers
 o.jumpoptions = "stack" -- More intuitive jumps
 opt.diffopt:append("algorithm:histogram", "inline:word") -- Improve diff mode
 opt.shortmess:append("A") -- Suppress swapfile warnings
+
+if vim.fn.has("nvim-0.12") == 1 then
+  opt.fillchars:append({
+    fold = " ",
+    foldclose = "",
+    foldinner = " ",
+    foldopen = "",
+    foldsep = " ",
+  })
+  o.statuscolumn = "%s%l%C "
+  o.foldcolumn = "1"
+end
